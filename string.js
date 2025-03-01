a15797549581b3ad23c0b2c34a295aea29389641
vaishuchowdary <vaishuchowdary039@gmail.com>
2025-03-01 12:30:10 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,12 @@
+let name ="RAVINDRA DEGREE COLLEGE FOR WOMEN"
+let splitName = name.substring(0,3)  
+let splitName1 = name.slice(0,3)  
+
+const newStringOne = " RAVINDRA DEGREE COLLEGE FOR WOMEN   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+ string1 = "vaishuchowdary"
+ string2 ="Binduchowdary"
+ sumOfString1String2 = string1 +" "+ string2
+console.log(sumOfString1String2)
