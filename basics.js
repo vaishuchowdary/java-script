4f583d32d832a7f40e84453391bcc4f4c99f4810
vaishuchowdary <vaishuchowdary039@gmail.com>
2025-03-01 12:29:50 +0530

    Add files via upload

---
+++ basics.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "vaishuchowdary039@gmail.com"
+var accountPassword = "praveenkumar97"
+accountCity = "Anatapuram"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "vaishuchowdary039@gmail.com"
+accountPassword ="praveenkumar97"
+accountCity = "Anantapur"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
